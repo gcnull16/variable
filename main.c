8f5c9a736b2e4c773ee26cb707251f908903f4ec
Null <gn439816@ohio.edu>
2018-09-13 18:43:47 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+   //variable declaration and assignment
+{
+
+
+int a;
+
+a = 445;
+
+//variable declaration and initialization
+int b = 4;
+
+int c = a;
+
+
+printf("value of a = %d\n", a);
+printf("value of b = %d\n", b);
+printf("value of c = %d\n", c);
+    //printf("Hello world!\n");
+
+
+
+    return 0;
+}
